fda729b6127eac06ec082507b1b5a6c567fabee7
EricB-59 <ebaenac59@gmail.com>
2025-02-24 18:37:02 +0100

    leson

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import FormularioRegistro from "./App.tsx";
+import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <FormularioRegistro titulo="Formulario no entiendo nada" />
+    <App />
   </StrictMode>
 );
