71bd545a37515e187e4d91737f05f1b595b7f345
EricB-59 <ebaenac59@gmail.com>
2025-02-24 18:39:49 +0100

    delete

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import FormularioRegistro from "./App.tsx";
-import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(document.getElementById("root")!).render(<StrictMode></StrictMode>);
